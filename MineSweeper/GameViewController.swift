23c31bfb2365c2b1acef22ea9bdab72795aacbf5
Mohamed Mostafa <mohameds.mostafas7@gmail.com>
2021-07-13 05:56:09 +0200

    add app icon --- add timer and bombs counter in the navigation bar --- add button drag func to mark the button as a bomb --- add animation while showing the bomb after losing --- make the color of the number of touched bombs unique.

--- MineSweeper/GameViewController.swift
+++ MineSweeper/GameViewController.swift
@@ -10,14 +10,24 @@ import UIKit
 class GameViewController: UIViewController {
     var numberOfRows: Int?
     var numberOfBombs: Int?
+    var remainingBombsNumber = 0 {
+        didSet {
+            bombs.text = "\(remainingBombsNumber) 💣"
+        }
+    }
     var numberOfColumns: Int?
-    var counter = 0
+    var smashedBombsCounter = 0
     var buttons = [UIButton]()
     let buttonsView = UIView()
     var navigationBarHeight: CGFloat = 0.0
     var bombsArray = [Bool]()
     var touchedBombs = [Int]()
+    var gameTimer: Timer?
     
+    var time = UILabel()
+    var bombs = UILabel()
+    // this is the timer count
+    var secondsCounter = 0
     
     // MARK: - Create ButtonsView
     override func loadView() {
@@ -40,13 +50,34 @@ class GameViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        remainingBombsNumber = numberOfBombs ?? 0
+        
+        // MARK: - Timer
+        startTimer()
+        
+        // MARK: - navigationBar items
+        if let navigationBar = self.navigationController?.navigationBar {
+            let remainingBombs = CGRect(x: navigationBar.frame.width - 60, y: 0, width: 60, height: navigationBar.frame.height)
+            let timer = CGRect(x: navigationBar.frame.width/2 - 30, y: 0, width: 60, height: navigationBar.frame.height)
+
+            bombs = UILabel(frame: remainingBombs)
+            bombs.textAlignment = .center
+            bombs.text = "\(remainingBombsNumber) 💣"
+
+            time = UILabel(frame: timer)
+            time.textAlignment = .left
+            time.text = "00:00"
+
+            navigationBar.addSubview(bombs)
+            navigationBar.addSubview(time)
+        }
         
     }
     
     
     override func viewDidLayoutSubviews() {
-        // MARK: - Add The Buttons To The ButtonsView
         
+        // MARK: - Add The Buttons To The ButtonsView
         guard let c = numberOfColumns, let r = numberOfRows else { return }
         let width = view.frame.size.width  / CGFloat(c)
         let height = buttonsView.frame.size.height / CGFloat(r)
@@ -55,20 +86,16 @@ class GameViewController: UIViewController {
             for row in 0..<row {
                 for column in 0..<column {
                     let explodingButton = UIButton(type: .system)
-                    explodingButton.setTitle("", for: .normal)
                     explodingButton.addTarget(self, action: #selector(buttonTapped), for: .touchUpInside)
-                    
-                    // MARK: - long press gesture
-                    let longPressGeusture = UILongPressGestureRecognizer(target: self, action: #selector(longPress))
-                    explodingButton.addGestureRecognizer(longPressGeusture)
+                    explodingButton.addTarget(self, action: #selector(buttonDraged(_:)), for: .touchDragExit)
                     
                     let frame = CGRect(x: column * Int(width), y: row * Int(height), width: Int(width), height: Int(height))
                     explodingButton.frame = frame
+                    explodingButton.setTitle("", for: .normal)
                     explodingButton.layer.borderWidth = 1
                     explodingButton.layer.borderColor = UIColor.gray.cgColor
                     explodingButton.layer.cornerRadius = width / 10
                     explodingButton.backgroundColor = tag % 2 == 0 ? .gray : .lightGray
-                    explodingButton.setTitleColor(.red, for: .normal)
                     explodingButton.layer.shadowOpacity = 1.0
                     explodingButton.layer.shadowColor = UIColor.white.cgColor
                     explodingButton.tag = tag
@@ -81,32 +108,78 @@ class GameViewController: UIViewController {
         }
     }
     
-    // MARK: - long tap
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        gameTimer?.invalidate()
+        time.removeFromSuperview()
+        bombs.removeFromSuperview()
+    }
     
-    @objc func longPress(_ guesture: UILongPressGestureRecognizer, _ sender: UIButton) {
+    
+    func startTimer() {
         
-        if guesture.state == UIGestureRecognizer.State.began {
-            print("Long Press")
-            print(sender.tag)
-//            buttons[sender.tag].isEnabled = false
+        gameTimer?.invalidate()
+        gameTimer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true) { timer in
+            self.secondsCounter += 1
+            let seconds = self.secondsCounter%60
+            let minutes = self.secondsCounter/60
+            var stringSeconds = "\(seconds)"
+            var stringMinutes = "\(minutes)"
+            
+            if seconds < 10 {
+                stringSeconds = "0\(seconds)"
+            }
+            if minutes < 10 {
+                stringMinutes = "0\(minutes)"
+            }
+            self.time.text = "\(stringMinutes):\(stringSeconds)"
             
         }
     }
+
+    @objc func buttonDraged(_ sender: UIButton) {
+        
+        if sender.currentTitle == "" {
+            if remainingBombsNumber == 0 {
+                let ac = UIAlertController(title: "Be carful ‼", message: "you have already used all the mark", preferredStyle: .alert)
+                let Ok = UIAlertAction(title: "Ok", style: .default)
+                ac.addAction(Ok)
+                present(ac, animated: true)
+                return
+            }
+            sender.setTitle("🚩", for: .normal)
+            remainingBombsNumber -= 1
+        } else {
+            sender.setTitle("", for: .normal)
+            remainingBombsNumber += 1
+        }
+        
+    }
+    
     // MARK: - buttonTapped
     
     @objc func buttonTapped(_ sender: UIButton) {
+        print(sender.currentTitle ?? "blablabla")
+        guard sender.currentTitle != "🚩" else {return}
         let button = buttons[sender.tag]
         
         // in first click
-        if counter == 0 {
+        if smashedBombsCounter == 0 {
             firstClick(buttonTag: button.tag)
         }
         
         // check if it's a bomb or not
         if bombsArray[button.tag] == true {
-            button.backgroundColor = .red
-            button.setTitle("💣", for: .normal)
+            for i in 0..<bombsArray.count {
+                if bombsArray[i] == true {
+                    UIView.animate(withDuration: 2, delay: 0.1, options: .curveEaseInOut, animations: { [self] in
+                        buttons[i].backgroundColor = .red
+                        buttons[i].setTitle("💣", for: .normal)
+                    }, completion: nil)
+                }
+            }
             gameOver(title: "You Lost 😔")
+            bombs.text = "😢"
         } else {
             explodeButton(button: button)
         }
@@ -114,9 +187,11 @@ class GameViewController: UIViewController {
         button.layer.cornerRadius = 0
     }
     
+    // MARK: - explode func
     // this func is to find explode the neighor of a button if it has no touched bomb
     // and explode only the button if it has touched bomb
     func explodeButton(button: UIButton) {
+        guard button.currentTitle != "🚩" else {return}
         if touchedBombs[button.tag] == 0 {
             smash(button: button)
             let neighbors = CheckNeighbors(buttonTag: button.tag)
@@ -126,38 +201,43 @@ class GameViewController: UIViewController {
                 } else {
                     smash(button: buttons[neighbor])
                 }
-                
             }
         } else {
             smash(button: button)
         }
-        
-        
     }
     
+    // MARK: - smash func
     // disable the button and add the number of touched bombs to it
     func smash(button: UIButton) {
         guard button.isEnabled else { return }
-        counter += 1
+        smashedBombsCounter += 1
         button.isEnabled = false
         button.backgroundColor = button.tag %  2 == 0 ? .white :  UIColor(red: 250/255, green: 230/255, blue: 243/255, alpha: 1)
         
+        // add the number of touched bombs with special color
         switch touchedBombs[button.tag] {
         case 0:
             button.setTitle("", for: .normal)
-        case 1,2:
+            
+        case 1:
             button.setTitle("\(touchedBombs[button.tag])", for: .normal)
             button.setTitleColor(.blue, for: .normal)
-        case 3,4:
+            
+        case 2:
             button.setTitle("\(touchedBombs[button.tag])", for: .normal)
             button.setTitleColor(.orange, for: .normal)
+        case 3,4:
+            button.setTitle("\(touchedBombs[button.tag])", for: .normal)
+            button.setTitleColor(.brown, for: .normal)
         default:
             button.setTitle("\(touchedBombs[button.tag])", for: .normal)
             button.setTitleColor(.red, for: .normal)
         }
         
-        if counter == buttons.count - (numberOfBombs ?? 0) {
+        if smashedBombsCounter == buttons.count - (numberOfBombs ?? 0) {
             gameOver(title: "Congratulations You Won 🥳")
+            bombs.text = "😃"
         }
         
     }
@@ -173,11 +253,11 @@ class GameViewController: UIViewController {
         }
         bombsArray.shuffle()
         
-        print(bombsArray.count)
     }
     
     // MARK: - When Game Is Over
     func gameOver(title: String){
+        gameTimer?.invalidate()
         let ac = UIAlertController(title: "\(title)", message: nil, preferredStyle: .alert)
         ac.addAction(UIAlertAction(title: "Restart", style: .default, handler: { _ in
             self.restart()
@@ -191,13 +271,16 @@ class GameViewController: UIViewController {
     
     // MARK: - Restart Func
     func restart() {
+        secondsCounter = -1
+        remainingBombsNumber = numberOfBombs ?? 0
+        startTimer()
         for button in buttons {
             button.isEnabled = true
             button.backgroundColor = button.tag %  2 == 0 ? .gray : .lightGray
             button.setTitle("", for: .normal)
             bombsArray.shuffle()
         }
-        counter = 0
+        smashedBombsCounter = 0
     }
     
     // MARK: - Check neiboughrs
@@ -245,6 +328,7 @@ class GameViewController: UIViewController {
     }
     
     // MARK: - Add bombs in the first click
+    // In the first click the clicked button must contain no bomb inside or the and the touching buttons as well.
     func firstClick(buttonTag: Int) {
         touchedBombs = [Int](repeating: 0, count: buttons.count)
         var neighbors = CheckNeighbors(buttonTag: buttonTag)
